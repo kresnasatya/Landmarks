da8774aa34ca1ebdad0730eeb530fca1333bce17
Kresna Satya <kresnasatya72@gmail.com>
2024-08-05 08:20:23 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kresna Satya on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
