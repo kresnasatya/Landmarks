360c4912831938241f8ac8d029285059d856d127
Kresna Satya <kresnasatya72@gmail.com>
2024-08-06 07:42:12 +0800

    SwiftUI Tutorials: Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kresna Satya on 05/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
